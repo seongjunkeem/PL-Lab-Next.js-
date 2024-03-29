87d0923d9ce2f59d765a13511b1644b4b2600911
{onegyeol} <chlgksruf3414@gmail.com>
2024-03-29 15:43:42 +0900

    chapter3

---
+++ greet/[name].js
@@ -0,0 +1,17 @@
+export async function getServerSideProps({params}){
+    const {name} = params;
+    return{
+        props : {
+            name,
+        },
+    };
+}
+
+
+function Greet(props){
+    return(
+        <h1> Hello, {props.name}! </h1>
+    );
+}
+
+export default Greet;
---
+++ index.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import Link from "next/link";
+import Widget from '../components/Widget';
+
+function IndexPage(){
+  return(
+    <>
+    <Head>
+      <title> Welcome to my Next.js website</title>
+    </Head>
+    <div>
+      <Link href='/about' passHref>About us</Link>
+    </div>
+    </>
+  );
+}
+
+export default IndexPage;
